ba869f3ac513d5a4aa8257ef959fa757b468d869
Zachweiss20 <Zachary.weiss@arraytechinc.com>
2021-01-27 16:16:36 -0700

    fiished 4.13

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -5,19 +5,18 @@ const Blog = require('../models/blog')
 blogRouter.get('', async(request, response) => {
   blogs = await Blog.find({})
     response.json(blogs)
-    .catch(error => console.log(error))
 })
   
 blogRouter.post('', async(request, response) => {
   const blog = new Blog(request.body)
   response = await blog.save()
-  response.status(201).json(result)
+  response.status(201).end()
 })
 
 blogRouter.delete('/:id', async(request, response)=> {
-  const body = request.body
-  const bid = body.id
-  blogs = await Blog.findByIdAndDelete({id:bid})
+  const bid = String(request.params.id)
+  console.log(bid)
+  blogs = await Blog.findByIdAndDelete({'_id':bid})
   response.status(204).end()
 })
 
