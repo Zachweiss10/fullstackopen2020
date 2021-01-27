fffa81a290fa4f8c1015aecf637231cc634fd6ed
Zachweiss20 <Zachary.weiss@arraytechinc.com>
2021-01-27 13:30:57 -0700

    finished 4.1-4.2

--- part4/app.js
+++ part4/app.js
@@ -1,28 +1,35 @@
-const http = require('http')
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+
 
 //local imports
 const config = require('./utils/config')
-const blogRouter = require('./controllers/blogRoutes')
+const blogRouter = require('./controllers/blog')
 const middleware = require('./utils/middleware')
-require('dotenv'.config)
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
+//logger.info('connecting to ', process.env.MONGODB_URI)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then( ()=>{
+        logger.info('connected to MongoDB')
+    })
+    .catch( (error) => {
+        logger.error('error conencting to MongoDB:', error.message)
+    })
+
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use('/api/blogs', blogRouter)
+app.use(middleware.requestLogger)
 
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+module.exports = app
--- part4/index.js
+++ part4/index.js
@@ -4,7 +4,8 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const server = http.createServer(app)
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+require('dotenv').config()
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
