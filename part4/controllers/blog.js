ab5abbc70c57bf1ae620562da264b5ce1a292f68
Zachweiss20 <Zachary.weiss@arraytechinc.com>
2021-01-27 16:06:28 -0700

    finished 4.8

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -2,21 +2,23 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 
-blogRouter.get('', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    }).catch(error => console.log(error))
+blogRouter.get('', async(request, response) => {
+  blogs = await Blog.find({})
+    response.json(blogs)
+    .catch(error => console.log(error))
 })
   
-blogRouter.post('', (request, response) => {
+blogRouter.post('', async(request, response) => {
   const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    }).catch(error => console.log(error))
+  response = await blog.save()
+  response.status(201).json(result)
+})
+
+blogRouter.delete('/:id', async(request, response)=> {
+  const body = request.body
+  const bid = body.id
+  blogs = await Blog.findByIdAndDelete({id:bid})
+  response.status(204).end()
 })
 
 module.exports = blogRouter
